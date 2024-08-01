16dae4cf454f5c87f777fe555759f77d5e5a21eb
abdurahmonanvarov <dousolo9@gmail.com>
2024-08-01 12:15:24 +0500

    last

--- js/index.js
+++ js/index.js
@@ -2,36 +2,35 @@
 const name = document.getElementById('name');
 const password = document.getElementById('password');
 const button = document.getElementById('button');
-const button3 = document.getElementById('button2')
 
 
-function user(name, password){
-   if(name.value != 'Husanboy'){
-    alert('Ismingizni tugri kiriting');
-    name.style.outlineColor = 'red';
-    return false
-   }
+function user(name, password) {
+    if (name.value != 'Husanboy') {
+        alert('Ismingizni tugri kiriting');
+        name.style.outlineColor = 'red';
+        return false
+    }
 
-   if(password.value != 19051905){
-    alert('Men bergan kodni yoz');
-    return false;
-   }
+    if (password.value != 19051905) {
+        alert('Men bergan kodni yoz');
+        return false;
+    }
     return true;
 }
 
 
 
 
-button && button.addEventListener('click', function(event){
+button && button.addEventListener('click', function (event) {
     event.preventDefault();
 
-   isVali = user(name, password);
-   if(!isVali){
-    return;
-   }else{
-    let cut = window.location.href.split(' ');
-    window.location.assign(`${cut}/html/main.html`)
-   }
-   
+    isVali = user(name, password);
+    if (!isVali) {
+        return;
+    } else {
+        let cut = window.location.href.split(' ');
+        window.location.assign(`${cut}/html/main.html`)
+    }
+
 })
 
